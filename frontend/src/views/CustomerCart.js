786206ed4dc743aacd85b9f2e775f3729304cc4e
Bhavitha Innamuri <bhavithainnamuri@my.unt.edu>
2021-11-02 02:07:25 -0500

    Cart related changes

--- frontend/src/views/CustomerCart.js
+++ frontend/src/views/CustomerCart.js
@@ -1,5 +1,121 @@
-export default function CustomerCart() {
-    return(
-            "This is cart"
-    );
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react";
+import axios from "axios";
+import RecipeReviewCard from "../components/RecipeReviewCard";
+import Cookies from 'js-cookie';
+import FloatingActionButtons from '../components/FloatingButton';
+import CustomizedSnackbars from '../components/CustomizedSnackbars';
+import BasicTable from "../components/BasicTable";
+import CartTable from "../components/CartTable";
+
+
+export default class CustomerCart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        oldCart:{},
+      productsAll: [],
+      tableProd:[],
+      loggedin:false,
+      errAlert:''
+
+    };
+  }
+  
+ 
+  async componentDidMount ()  {
+
+    const Bearer = "Bearer "+ Cookies.get('token')
+    let axiosConfig = {
+     headers: {
+         'Content-Type': 'application/json;charset=UTF-8',
+         "Authorization" : Bearer
+     }
+   };
+ 
+          if(!Cookies.get('token')){
+          this.setState.loggedin = true
+          this.state.errAlert = "error"
+          this.state.message ="Only vendors can add products"
+         
+      }
+
+
+      axios.get(`http://localhost:5000/product/all`,axiosConfig).then(response => {console.log("response" + response.data);
+        this.setState({ productsAll: response.data });
+        console.log(this.state.productsAll,"check")
+        console.log(this.state.oldCart[0].productlist)
+
+        for(var i =0;i<this.state.productsAll.length;i++){
+    
+            for(var j=0;j<this.state.oldCart[0].productlist.length;j++){
+                
+              if(this.state.productsAll[i]._id == this.state.oldCart[0].productlist[j].product){
+                  var tempJson = {
+                      "id":this.state.productsAll[i]._id,
+                      "name": this.state.productsAll[i].name,
+                      "photo": this.state.productsAll[i].photo,
+                      "price": this.state.productsAll[i].price,
+                      "quantity": this.state.oldCart[0].productlist[j].quantity,
+                      "backQuantity":this.state.productsAll.quantity
+
+                  }
+                  this.setState({ tableProd: [...this.state.tableProd, tempJson] })
+
+              }
+              
+            }
+            console.log(this.state.tableProd,"tab")
+        }
+
+       
+      })
+      .catch(error => {
+          console.log(error)
+        
+      });
+      try{
+      
+      const hitback =  await axios.get("http://localhost:5000/customers/myCart",axiosConfig, {
+        withCredentials: true
+        
+    });
+
+    
+    console.log(hitback)
+    this.setState({ oldCart: hitback.data });
+    console.log("after hitback assignmemnt")
+    console.log(this.state.oldCart[0].productlist)
+    
+    console.log(this.state.productsAll)
+
+    // 
+    // this.setState({}) SET PRODTABLE DATA HERE
+}catch(e){
+       
+     
+    this.setState.loggedin = true
+    this.state.errAlert = "error"
+    this.state.message ="Only vendors can add products"
+    console.log("in error")
+    console.log(e)
+}
+console.log("before for",this.state.productsAll )
+
+
+
+
+  }
+
+  render() {
+      
+  
+      return(
+         
+    <div style={{ display: "inline-block" ,position:"relative",top:"50px",left:"110px"}}>
+    <CartTable products = {this.state.tableProd} />
+  </div>
+  )
+    
+  }
 }
--- frontend/src/views/CustomerHome.js
+++ frontend/src/views/CustomerHome.js
@@ -8,6 +8,7 @@ import theme from "../components/theme";
 import { ThemeProvider } from '@material-ui/core/styles';
 import withRoot from '../components/WithRoot';
 import { CssBaseline } from "@mui/material";
+import Cookies from 'js-cookie';
 
 
 
@@ -17,20 +18,96 @@ class CustomerHome extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: []
+      products: [],
+      oldCart:{},
+      oldCart:{},
+      productsAll: [],
+      tableProd:[],
+      loggedin:false,
+      errAlert:''
     };
   }
 
-  componentDidMount() {
+  componentDidMount () {
+    const Bearer = "Bearer "+ Cookies.get('token')
+    let axiosConfig = {
+     headers: {
+         'Content-Type': 'application/json;charset=UTF-8',
+         "Authorization" : Bearer
+     }
+   };
+ 
+          if(!Cookies.get('token')){
+          this.setState.loggedin = true
+          this.state.errAlert = "error"
+          this.state.message ="Only vendors can add products"
+         
+      }
+
+
     axios
       .get(`http://localhost:5000/product/all`)
       .then(response => {
         console.log("response" + response.data);
         this.setState({ products: response.data });
+        this.setState({ productsAll: response.data });
+
+
+        for(var i =0;i<this.state.productsAll.length;i++){
+    
+          for(var j=0;j<this.state.oldCart[0].productlist.length;j++){
+              
+            if(this.state.productsAll[i]._id == this.state.oldCart[0].productlist[j].product){
+                var tempJson = {
+                    "id":this.state.productsAll[i]._id,
+                    "name": this.state.productsAll[i].name,
+                    "photo": this.state.productsAll[i].photo,
+                    "price": this.state.productsAll[i].price,
+                    "quantity": this.state.oldCart[0].productlist[j].quantity,
+                    "backQuantity":this.state.productsAll.quantity
+
+                }
+                this.setState({ tableProd: [...this.state.tableProd, tempJson] })
+
+            }
+            
+          }
+          console.log(this.state.tableProd,"tab")
+      }
       })
       .catch(error => {
         console.log(error);
       });
+
+
+
+
+
+
+      try{
+      
+        axios.get("http://localhost:5000/customers/myCart",axiosConfig, {
+        withCredentials: true
+        
+    }).then(resposne =>{
+      this.setState({ oldCart: resposne.data });
+      console.log(this.state.oldCart[0].productlist)
+    })
+  
+      
+      
+  
+      // 
+      // this.setState({}) SET PRODTABLE DATA HERE
+  }catch(e){
+         
+       
+      this.setState.loggedin = true
+      this.state.errAlert = "error"
+      this.state.message ="Only vendors can add products"
+      console.log("in error")
+      console.log(e)
+  }
   }
 
 
@@ -51,7 +128,7 @@ class CustomerHome extends Component {
           <Grid container spacing={4} >
             {this.state.products.map(currentproduct => (
               <Grid item key={currentproduct} xs={3} >
-                  <RecipeReviewCard product={currentproduct} />
+                  <RecipeReviewCard oldCart={this.state.oldCart} product={currentproduct} />
               </Grid>
             ))}
           </Grid>
